84e9de4c3fe0617946b397771a1bc16c1390e29c
rikiTada <riki.tada777@gmail.com>
2023-08-07 21:27:32 +0900

    PROFILE>detail

--- src/components/organisms/AppProfile.vue
+++ src/components/organisms/AppProfile.vue
@@ -1,18 +1,41 @@
 <script setup>
 import AppMockup from "../molecules/AppMockup.vue";
 import AppSvg from "../Atoms/AppSvg.vue";
-import SkillList from "../Atoms/SkillList.vue";
+// import SkillList from "../Atoms/SkillList.vue";
 </script>
 <template>
   <h2 class="section_ttl">PROFILE</h2>
-  <div class="flex md:flex-row flex-col justify-center">
+  <div class="flex gap-8 md:flex-row flex-col justify-center">
     <AppSvg class="pointer-events-none" />
+    <!-- <img src="../../assets/photo.png" class="lg:w-1/2 w-auto" alt="" /> -->
     <div class="text-white tracking-widest">
       <h3 class="text-2xl font-bold hover:after:content-['🙌']">
         Hi, I'm Riki.
       </h3>
       <hr />
-      <SkillList
+      <div class="mt-4 [&>p]:my-3 font-normal">
+        <p>
+          東京在住のフロントエンダーです。<br />
+          趣味は、読書とお酒の’96年生まれ。デザインも大好きです。
+        </p>
+        <p>
+          幅広い知識と、能動的に動く姿勢から、<br />
+          職場では”雑食系男子”の称号をいただいています。
+        </p>
+        <p>前職はアイス屋の店長という一面もあります。</p>
+        <p>
+          仕事の早さ・精度・信頼性に自信がありますので、<br />
+          気になった方は、ぜひ
+          <span class="underline font-bold pr-1 hover:text-blue-600"
+            ><a href="#contact">Contact</a></span
+          >からご連絡ください。
+        </p>
+        <div>
+          <h4 class="font-semibold text-sm mt-5 mb-1">好きな言葉:</h4>
+          <p class="text-3xl font-semibold">効率・自動化・まったり</p>
+        </div>
+      </div>
+      <!-- <SkillList
         title="Languages I speak:"
         skills="HTML/CSS, JavaScript, PHP, Python"
       />
@@ -22,7 +45,7 @@ import SkillList from "../Atoms/SkillList.vue";
       <SkillList
         title="Other Skills:"
         skills="Tailwindcss, SCSS, StoryBook, Figma, WordPress, microCMS"
-      />
+      /> -->
     </div>
   </div>
   <AppMockup />
